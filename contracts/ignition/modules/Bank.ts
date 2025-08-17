7739ae471889ef7d278b885e2b4cdd16da019d96
negretenico <negretenico0@gmail.com>
2025-08-17 19:56:29 -0400

    Adding testnet contract

---
+++ contracts/ignition/modules/Bank.ts
@@ -0,0 +1,13 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("BankModule", (m) => {
+  const counter = m.contract("Bank");
+
+  m.call(counter, "deposit", [], {
+    value: 5_000n,
+  });
+  m.call(counter, "withdraw", [5n]);
+  m.call(counter, "getBalance", []);
+
+  return { counter };
+});
